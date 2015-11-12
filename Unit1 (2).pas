4e0a8aefcee56ce11a1237c762ce7bf53fb0ad7e
ValeriaKulikova <lera_kulikova@mail.ru>
2015-11-12 17:28:48 +0300

    динамическая библиотека

---
+++ Unit1 (2).pas
@@ -0,0 +1,57 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+Tlera = function ( r: real ): real; stdcall;
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  hndDLLHandle: THandle;
+  lera: Tlera;
+begin
+
+    // загружаем dll динамически
+    hndDLLHandle := loadLibrary ( 'Project2.dll' );
+    if hndDLLHandle <> 0 then
+    begin
+    // получаем адрес функции
+      @lera := getProcAddress ( hndDLLHandle, 'lera' );
+       // если адрес функции найден
+      if addr ( lera ) <> nil then
+      begin
+        // показываем результат ( 21...)
+        showMessage ( floatToStr ( lera ( 10.5 ) ) );
+      end
+       else
+        // DLL не найдена ("handleable")
+        showMessage ( 'Function not exists...' );
+
+    end
+    else
+      // DLL не найдена ("handleable")
+      showMessage ( 'DLL not found...' );
+      FreeLibrary( hndDLLHandle);
+  end;
+ 
+end.
+
---
+++ Unit1.~dfm
@@ -0,0 +1,25 @@
+object Form1: TForm1
+  Left = 189
+  Top = 125
+  Width = 870
+  Height = 640
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 120
+    Top = 56
+    Width = 273
+    Height = 113
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+end
